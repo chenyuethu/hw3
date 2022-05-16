7effae705d481f761d8d44555ad8f0e6836a26d1
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-05-16 21:20:14 +0000

    1

---
+++ scripts/create_data.rb
@@ -0,0 +1,46 @@
+Place.destroy_all
+Post.destroy_all
+
+mexico = Place.new
+mexico["name"] = "Mexico City"
+mexico.save
+
+charleston = Place.new
+charleston["name"] = "Charleston"
+charleston.save
+
+beijing = Place.new
+beijing["name"] = "Beijing"
+beijing.save
+
+place = Place.new
+place["name"] = "Amsterdam"
+place.save
+
+post = Post.new
+post["title"] = "Ate tacos"
+post["description"] = "Checked out Ate tacos."
+post["posted_on"] = "2022-01-01"
+post["place_id"] = mexico["id"]
+post.save
+
+post = Post.new
+post["title"] = "Ate tacos"
+post["description"] = "Checked out Ate tacos."
+post["posted_on"] = "2022-01-01"
+post["place_id"] = mexico["id"]
+post.save
+
+post = Post.new
+post["title"] = "Lisa and Jim's Wedding"
+post["description"] = "attended the wedding."
+post["posted_on"] = "2022-01-01"
+post["place_id"] = mexico["id"]
+post.save
+
+post = Post.new
+post["title"] = "Ate tacos again"
+post["description"] = "Checked out Ate tacos one more time."
+post["posted_on"] = "2022-01-01"
+post["place_id"] = mexico["id"]
+post.save
