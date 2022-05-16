970ce005bbcc8228b8386cabb90f7f481a7df365
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-05-16 21:34:38 +0000

    1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+        @place = Place.find_by({ "id" => @post["place_id"] })
+        @post["place_id"] = @place["id"]
+    end
+
+    def create
+        @post = Post.new
+        @contact["first_name"] = params["contact"]["first_name"]
+        @contact["last_name"] = params["contact"]["last_name"]
+        @contact["email"] = params["contact"]["email"]
+        @contact["phone_number"] = params["contact"]["phone_number"]
+        @contact["company_id"] = params["contact"]["company_id"]
+        @contact.save
+        redirect_to "/companies/#{@contact["company_id"]}"
+    end
+
 end
